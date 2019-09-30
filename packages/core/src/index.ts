9760a1b713bf78722667ed1d464f30807ce4ed4e
Wallace Breza <wallace@breza.me>
2019-09-30 09:05:05 -0700

    feat: Added start of userProfileApi + Lerna

---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./models/app";
+export * from "./services/categoryService";
+export * from "./services/pullRequestService";
+export * from "./services/repositoryService";
+export * from "./services/userProfileService";
---
+++ packages/userProfileApi/src/config.ts
@@ -0,0 +1,19 @@
+import {
+  LoggingServiceMiddleware,
+  HTTPBindingMiddleware,
+  PerformanceMiddleware,
+  ExceptionMiddleware,
+  ConsoleLogger,
+  LogLevel,
+} from "@multicloud/sls-core";
+
+const defaultLogger = new ConsoleLogger(LogLevel.VERBOSE);
+
+export const config = () => {
+  return [
+    LoggingServiceMiddleware(defaultLogger),
+    PerformanceMiddleware(),
+    ExceptionMiddleware({ log: defaultLogger.log as any }),
+    HTTPBindingMiddleware()
+  ];
+};
