b1c33d72ae4b7ec3533a4fb656a67d6d681b1dd3
Wallace Breza <wallace@breza.me>
2019-10-01 17:47:37 -0700

    Filled out put/patch/delete APIs for user profile and repo

--- packages/userProfileApi/src/app.ts
+++ packages/userProfileApi/src/app.ts
@@ -1,4 +1,9 @@
-import { App } from "@multicloud/sls-core";
+import { App, CloudContext } from "@multicloud/sls-core";
 import { AzureModule } from "@multicloud/sls-azure";
+import { UserProfile } from "@paddleboard/core";
+
+export interface UserProfileApiContext extends CloudContext {
+  user: UserProfile;
+}
 
 export const app = new App(new AzureModule);
--- packages/userProfileApi/src/handlers/userProfile.ts
+++ packages/userProfileApi/src/handlers/userProfile.ts
@@ -1,34 +1,62 @@
-import { app } from "../app";
+import { app, UserProfileApiContext } from "../app";
 import { config } from "../config"
-import { CloudContext } from "@multicloud/sls-core";
 import { UserProfileService, UserValidationMiddleware } from "@paddleboard/core";
 
 const middlewares = config();
 const userValidation = UserValidationMiddleware();
 
-export const getUserProfileList = app.use(middlewares, async (context: CloudContext) => {
+export const getUserProfileList = app.use(middlewares, async (context: UserProfileApiContext) => {
   const userService = new UserProfileService();
   const users = await userService.list();
 
   context.send({ value: users }, 200);
 });
 
-export const getUserProfile = app.use([...middlewares, userValidation], (context: CloudContext) => {
-  context.send({ value: context["user"] }, 200);
+export const getUserProfile = app.use([...middlewares, userValidation], (context: UserProfileApiContext) => {
+  context.send({ value: context.user }, 200);
 });
 
-export const postUserProfile = app.use(middlewares, (context: CloudContext) => {
-  context.send([], 201);
+export const postUserProfile = app.use(middlewares, async (context: UserProfileApiContext) => {
+  if (!context.req.body) {
+    return context.send({ message: "user profile is required" }, 400);
+  }
+
+  const userService = new UserProfileService();
+  const user = await userService.save(context.req.body);
+  const newUri = `users/${user.id}`;
+
+  context.res.headers.set("location", newUri);
+  context.send({ value: user }, 201);
 });
 
-export const putUserProfile = app.use(middlewares, (context: CloudContext) => {
+export const putUserProfile = app.use([...middlewares, userValidation], async (context: UserProfileApiContext) => {
+  const userToSave = {
+    ...context.req.body,
+    id: context.user.id
+  };
+
+  const userService = new UserProfileService();
+  await userService.save(userToSave);
+
+  context.send(null, 204);
+});
+
+export const patchUserProfile = app.use([...middlewares, userValidation], async (context: UserProfileApiContext) => {
+  const userToSave = {
+    ...context.user,
+    ...context.req.body,
+    id: context.user.id
+  };
+
+  const userService = new UserProfileService();
+  await userService.save(userToSave);
+
   context.send([], 204);
 });
 
-export const patchUserProfile = app.use(middlewares, (context: CloudContext) => {
-  context.send([], 204);
-});
+export const deleteUserProfile = app.use([...middlewares, userValidation], async (context: UserProfileApiContext) => {
+  const userService = new UserProfileService();
+  await userService.delete(context.user.id);
 
-export const deleteUserProfile = app.use(middlewares, (context: CloudContext) => {
-  context.send([], 204);
+  context.send(null, 204);
 });
