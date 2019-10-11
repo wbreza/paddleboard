ab5368177d489c69b55f3107d08f06d2a4803312
Wallace Breza <wallace@breza.me>
2019-10-11 11:59:43 -0700

    Updated paddleboard logo

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/logo512.png" className="App-logo" alt="logo" />
         <p>
           Welcome to Paddleboard.
         </p>
