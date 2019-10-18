de4327717f0e7a2fcd9b8b4c0a42eabef5d3c6f4
Wallace Breza <wallace@breza.me>
2019-10-18 09:28:30 -0700

    Updated publish script

--- packages/core/scripts/publish.sh
+++ packages/core/scripts/publish.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -eo pipefail
 
+PACKAGE_NAME=$1
+
 # set up .npmrc to authenticate with the provided token
-echo "Set up .npmrc ..."
+echo "Seting up .npmrc ..."
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 
-# NOTE: auth is taken care of via AzDO `npm auth` task. ENV vars would work as well.
-if [ -z "$1" ]; then
+if [ -z "$2" ]; then
   echo "Publishing 'latest' to NPM...";
-  npm publish $(System.ArtifactsDirectory)/core/paddleboard-core-1.0.0.tgz --access public
+  npm publish ${PACKAGE_NAME} --access public
 else
   echo "Publishing 'prerelease' to NPM...";
-  npm publish $(System.ArtifactsDirectory)/core/paddleboard-core-1.0.0.tgz --tag=beta --access public
+  npm publish ${PACKAGE_NAME} --tag=beta --access public
 fi
